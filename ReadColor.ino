8e3bc3e33c173148b0e9d71456d1b3b66368e28f
Sid Hillwig <sidneyhillwig@gmail.com>
2019-12-14 16:07:55 -0500

    Add files via upload
    Collecting code from Arduino

---
+++ ReadColor.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>
+#include <SFE_ISL29125.h>
+
+
+// Declare sensor, servomotors, and incoming data from MATLAB
+SFE_ISL29125 RGB_sensor;
+
+
+void setup() {
+  // Initialize serial communication
+  Serial.begin(115200);
+
+  // Initialize the ISL29125 with simple configuration so it starts sampling
+  if (RGB_sensor.init()); {
+    Serial.println("Sensor Initialization Successful\n\r");
+  }
+  
+}
+
+// Read sensor values for each color and print them to serial monitor
+void loop() {
+  // Read sensor values (16 bit integers)
+  unsigned int R = RGB_sensor.readRed();
+  Serial.println(R);
+  unsigned int G = RGB_sensor.readGreen();
+  Serial.println(G);
+  unsigned int B = RGB_sensor.readBlue();
+  Serial.println(B);
+  
+  delay(1000);
+}
+
